034401af0bfa96834e38bc4a37ce0f2bb17912bb
Ryan Miller <miller.ryan2005@gmail.com>
2026-04-23 19:56:25 -0700

    Uploaded Homework11 Files

---
+++ Homework11/Chapter22/Challenge4/common.h
@@ -0,0 +1,15 @@
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
+
+#define UINT8_COUNT (UINT8_MAX + 1)
+#define UINT16_COUNT (UINT16_MAX + 1)
+
+#endif
+
---
+++ Homework11/Chapter22/Challenge4/vm.h
@@ -0,0 +1,32 @@
+#ifndef CLOX_VM_H
+#define CLOX_VM_H
+
+#include "chunk.h"
+#include "table.h"
+#include "value.h"
+
+#define STACK_MAX UINT16_COUNT
+
+typedef struct {
+  Chunk* chunk;
+  uint8_t* ip;
+  Value stack[STACK_MAX];
+  Value* stackTop;
+  Table globals;
+  Table strings;
+  Obj* objects;
+} VM;
+
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+extern VM vm;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(const char* source);
+
+#endif
