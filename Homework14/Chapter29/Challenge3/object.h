c3e04aa1e6a9b7fb44f94e0085baa41abf094a0c
Ryan Miller <miller.ryan2005@gmail.com>
2026-05-11 15:13:34 -0700

    Uploaded Homework14 Files

---
+++ Homework14/Chapter29/Challenge3/object.h
@@ -0,0 +1,120 @@
+#ifndef CLOX_OBJECT_H
+#define CLOX_OBJECT_H
+
+#include "common.h"
+#include "chunk.h"
+#include "table.h"
+#include "value.h"
+
+typedef struct ObjString ObjString;
+typedef struct ObjUpvalue ObjUpvalue;
+
+typedef enum {
+  OBJ_BOUND_METHOD,
+  OBJ_CLASS,
+  OBJ_CLOSURE,
+  OBJ_FUNCTION,
+  OBJ_INSTANCE,
+  OBJ_NATIVE,
+  OBJ_UPVALUE,
+  OBJ_STRING,
+} ObjType;
+
+struct Obj {
+  ObjType type;
+  bool isMarked;
+  struct Obj* next;
+};
+
+typedef struct {
+  Obj obj;
+  ObjString* name;
+  uint16_t id;
+  Table methods;
+} ObjClass;
+
+typedef struct {
+  Obj obj;
+  ObjClass* klass;
+  Table fields;
+} ObjInstance;
+
+typedef struct {
+  Obj obj;
+  int arity;
+  int upvalueCount;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
+
+typedef Value (*NativeFn)(int argCount, Value* args);
+
+typedef struct {
+  Obj obj;
+  NativeFn function;
+} ObjNative;
+
+struct ObjString {
+  Obj obj;
+  int length;
+  char* chars;
+  uint32_t hash;
+};
+
+struct ObjUpvalue {
+  Obj obj;
+  Value* location;
+  Value closed;
+  ObjUpvalue* next;
+};
+
+typedef struct {
+  Obj obj;
+  ObjFunction* function;
+  ObjUpvalue** upvalues;
+  int upvalueCount;
+  // If this closure is a method, the ID of the class that declares it.
+  uint16_t classID;
+} ObjClosure;
+
+typedef struct {
+  Obj obj;
+  Value receiver;
+  ObjClosure* method;
+} ObjBoundMethod;
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#define IS_BOUND_METHOD(value) isObjType(value, OBJ_BOUND_METHOD)
+#define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
+#define IS_CLASS(value) isObjType(value, OBJ_CLASS)
+#define IS_INSTANCE(value) isObjType(value, OBJ_INSTANCE)
+#define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_BOUND_METHOD(value) ((ObjBoundMethod*)AS_OBJ(value))
+#define AS_CLASS(value) ((ObjClass*)AS_OBJ(value))
+#define AS_CLOSURE(value) ((ObjClosure*)AS_OBJ(value))
+#define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
+#define AS_INSTANCE(value) ((ObjInstance*)AS_OBJ(value))
+#define AS_NATIVE(value) (((ObjNative*)AS_OBJ(value))->function)
+#define AS_STRING(value) ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
+
+ObjBoundMethod* newBoundMethod(Value receiver, ObjClosure* method);
+ObjClass* newClass(ObjString* name, uint16_t id);
+ObjClosure* newClosure(ObjFunction* function);
+ObjFunction* newFunction();
+ObjInstance* newInstance(ObjClass* klass);
+ObjNative* newNative(NativeFn function);
+ObjUpvalue* newUpvalue(Value* slot);
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
+void printObject(Value value);
+
+#endif
